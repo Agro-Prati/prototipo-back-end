db94f0585c756fb93a0e58ee966c92f16e46bc8d
leandro <leandro_antunes28@hotmail.com>
2025-08-08 10:18:27 -0300

    initial project

---
+++ src/main/java/com/agromaisprati/prototipobackagrospring/PrototipoBackAgroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.agromaisprati.prototipobackagrospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototipoBackAgroSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototipoBackAgroSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agromaisprati/prototipobackagrospring/PrototipoBackAgroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agromaisprati.prototipobackagrospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototipoBackAgroSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
