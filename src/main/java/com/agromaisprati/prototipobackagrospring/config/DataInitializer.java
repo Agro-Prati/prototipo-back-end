8c20c6ce9ea98657a1cbdca521d222c4a46f868a
leandro <leandro_antunes28@hotmail.com>
2025-10-25 20:10:00 -0300

    ajustando variaveis ambiente

--- src/main/java/com/agromaisprati/prototipobackagrospring/config/DataInitializer.java
+++ src/main/java/com/agromaisprati/prototipobackagrospring/config/DataInitializer.java
@@ -6,14 +6,17 @@ import com.agromaisprati.prototipobackagrospring.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 /**
  * Inicializa usuários de exemplo para cada tipo de profissão
+ * ATENÇÃO: Este componente só é ativado no profile 'dev' para desenvolvimento/testes
  */
 @Slf4j
 @Component
+@Profile("dev")
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
